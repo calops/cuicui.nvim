b2b0fdce9487e2434ad4c841465f867c040b4207
Rémi Labeyrie <calops@tocards.net>
2024-01-04 12:05:57 +0100

    tmp

---
+++ lua/cuicui.lua
@@ -0,0 +1,14 @@
+--- cuicui plugin for neovim
+
+---@class CuicuiSpec
+
+---@class CuicuiOptions
+---@field statusline CuicuiSpec
+
+---Setup and run cuicui plugin
+---@param opts CuicuiOptions
+local function setup_plugin(opts) end
+
+return {
+	setup = setup_plugin,
+}
